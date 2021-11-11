00a711cd3fadaee2b774ae1a885970b4ae28b544
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-11-12 00:14:28 +0300

    finished side bar navigation

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,6 +5,28 @@ import "./style.css";
 export const Nav = () => {
   return (
     <div className="navWrapper">
+      {/* <div className="navList"> */}
+      <ul>
+        <li>
+          <a href="./../Home">Home</a>
+        </li>
+        <li>
+          <a>Songs</a>
+        </li>
+        <li>
+          <a>Podcasts</a>
+        </li>
+        <li>
+          <a>Music Videos</a>
+        </li>
+        <li>
+          <a>Audio Books</a>
+        </li>
+        <li>
+          <a>Movies</a>
+        </li>
+      </ul>
+      {/* </div> */}
 
       {/* <Link to="/media/song" > song </Link> */}
     </div>
